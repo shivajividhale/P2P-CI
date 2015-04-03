e79242c22c43b41303e425e5c1d51370eca82445
shivajividhale <shivajividhale@gmail.com>
2015-04-03 16:38:52 -0400

    Entries getting into database

--- centralServer.py
+++ centralServer.py
@@ -1,5 +1,9 @@
 import socket
 import threading
+
+database = dict()
+rfc = '0'
+database.setdefault(rfc,[])
 def make_connection( port, addr):
     server_port = port
     client_address = addr
@@ -13,29 +17,39 @@ def make_connection( port, addr):
         if rfc_list:
             print("Hi: "+str(client_addr))
             print(rfc_list.decode("UTF-8"))
+            s = str(rfc_list.decode('UTF-8')).split()
+            print(s)
+            rfc = s[0]
+            if rfc in database:
+                print(database[rfc])
+            else:
+                database.setdefault(rfc,[])
+                database[rfc].append(client_addr)
+                print("Added to database for RFC:"+rfc+"Peer:"+str(database[rfc]))
         #client_socket.listen(1)
         #while True:
         #msg = client_socket.recv(100)
         #if not msg:
         #    break
-
         #print(msg.decode('UTF-8'))
+def main():
+    s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+    s.bind(('localhost',7734))
+    s.listen(5)
+    port = 9000
+    while True:
 
-s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-s.bind(('localhost',7734))
-s.listen(5)
-port = 9000
-while True:
+        (conn, addr) = s.accept()
+        print("Received a connection from:")
+        print(addr)
+        port = port +1
+        conn.send(bytes(str(port), "UTF-8"))
+        print("Sent:"+str(bytes(str(port), "UTF-8")))
+        print("Sent port number:"+str(port))
+        t = threading.Thread(name = addr, target = make_connection, args=( port, addr,))
+        t.start()
+        #rfc_list = conn.recv(100)
+        #print(rfc_list.decode("UTF-8"))
 
-    (conn, addr) = s.accept()
-    print("Received a connection from:")
-    print(addr)
-    port = port +1
-    conn.send(bytes(str(port), "UTF-8"))
-    print("Sent:"+str(bytes(str(port), "UTF-8")))
-    print("Sent port number:"+str(port))
-    t = threading.Thread(name = addr, target = make_connection, args=( port, addr,))
-    t.start()
-
-    #rfc_list = conn.recv(100)
-    #print(rfc_list.decode("UTF-8"))
+if __name__ == '__main__':
+    main()
